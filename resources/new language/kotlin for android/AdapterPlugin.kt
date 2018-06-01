e7d68059f9cb0bed5598c3c2143cae7a3800a058
Fay Ho <498130877@qq.com>
2018-06-01 16:46:31 +0800

    Add new language demo
    1. Add Kotlin for Android.
    2. Add Swift for iOS.

---
+++ resources/new language/kotlin for android/AdapterPlugin.kt
@@ -0,0 +1,118 @@
+/**
+ *
+ * Copyright (c) 2018 faylib.top
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+package top.faylib.oco.plugins
+
+import android.util.Log
+
+import org.apache.cordova.CallbackContext
+import org.apache.cordova.CordovaPlugin
+import org.apache.cordova.PluginResult
+import org.json.JSONArray
+import org.json.JSONObject
+
+class AdapterPlugin : CordovaPlugin() {
+
+    //region Variable
+
+    // 调试模式
+    private var debugMode = false
+
+    //endregion
+
+
+    //region Private Methods
+
+    // 打印调试信息
+    private fun debugLog(vararg strings: String) {
+        if (debugMode) {
+            for (string in strings) {
+                Log.i("OcO", "[ OcO ][ NETWORK ][ DEBUG ]$string.")
+            }
+        }
+    }
+
+    //endregion
+
+
+    //region Cordova Plugin Methods (JavaScript -> Java)
+
+    override fun execute(action: String?, args: JSONArray?, callbackContext: CallbackContext?): Boolean {
+
+        // Web 调用 -> 调试模式开关
+        if ("debug_mode" == action) {
+            debugMode = args!![0] as Boolean
+            debugLog(" '$action' run", " Debug Mode Open")
+            return true
+        }
+
+        // Web 调用 -> 关闭 Web 页面
+        else if ("dismiss_web" == action) {
+            cordova.activity.finish()
+            webDismissed()
+            debugLog(" '$action' run")
+            return true
+        }
+
+        return super.execute(action, args, callbackContext)
+    }
+
+    //endregion
+
+
+    //region Cordova Plugin Methods (Java -> JavaScript)
+
+    // Android 调用 -> Web 页面已关闭
+    private fun webDismissed() {
+        cordova.activity.runOnUiThread { webView.loadUrl("javascript:web_dismissed()") }
+    }
+
+    //endregion
+
+
+    //region CordovaPlugin Methods
+
+    /**
+     * 发送到 Web 的回调
+     *
+     * @param status 响应状态
+     * @param message 回调参数
+     * @param keepCallback 保持回调持续可用
+     * @param callbackContext 回调信息
+     */
+    private fun send(status: PluginResult.Status, message: Any, keepCallback: Boolean, callbackContext: CallbackContext) {
+        var pluginResult: PluginResult? = null
+        when (message) {
+            is String -> pluginResult = PluginResult(status, message)
+            is JSONArray -> pluginResult = PluginResult(status, message)
+            is JSONObject -> pluginResult = PluginResult(status, message)
+            is Int -> pluginResult = PluginResult(status, message)
+            is Float -> pluginResult = PluginResult(status, message)
+        }
+        pluginResult!!.keepCallback = keepCallback
+        callbackContext.sendPluginResult(pluginResult)
+    }
+
+    //endregion
+}
---
+++ resources/new language/swift for ios/Adapter.swift
@@ -0,0 +1,96 @@
+//
+//  Copyright (c) 2018 faylib.top
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+@objc(OcOAdapter)
+class Adapter: CDVPlugin {
+    
+    // 调试模式
+    private var debugMode = false
+    
+    
+    // MARK: Private Methods
+    
+    // 打印调试信息
+    private func debugLog(_ strings: String ...) {
+        if debugMode {
+            for string in strings {
+                print("[ OcO ][ ADAPTER ][ DEBUG ]\(string).")
+            }
+        }
+    }
+    
+    
+    // MARK: Cordova Plugin Methods (JavaScript -> Objective-C)
+    
+    // Web 调用 -> 调试模式开关
+    func debug_mode(_ command: CDVInvokedUrlCommand) {
+        commandDelegate.run {
+            self.debugMode = command.arguments[0] as! Bool
+            self.debugLog(" '\(#function)' run", " Debug Mode Open")
+        }
+    }
+    
+    // Web 调用 -> 关闭 Web 页面
+    func dismiss_web(_ command: CDVInvokedUrlCommand) {
+        viewController.dismiss(animated: true, completion: nil)
+        commandDelegate.run {
+            self.webDismissed()
+            self.debugLog(" '\(#function)' run", " Debug Mode Open")
+        }
+    }
+    
+    
+    // MARK: Cordova Plugin Methods (Objective-C -> JavaScript)
+    
+    // iOS 调用 -> Web 页面已关闭
+    func webDismissed() {
+        commandDelegate.evalJs("web_dismissed()")
+    }
+    
+    
+    // MARK: CDVPlugin Methods
+    
+    // 发送到 Web 的回调
+    func send(status: CDVCommandStatus, message:Any, command: CDVInvokedUrlCommand) {
+        send(status: status, message: message, keepCallback: false, command: command)
+    }
+    
+    // 发送到 Web 的回调
+    func send(status: CDVCommandStatus, message:Any, keepCallback: Bool, command: CDVInvokedUrlCommand) {
+        var pluginResult: CDVPluginResult!
+        if message is String {
+            pluginResult = CDVPluginResult(status: status, messageAs: message as! String)
+        } else if message is Array<Any> {
+            pluginResult = CDVPluginResult(status: status, messageAs: message as! Array)
+        } else if message is Dictionary<String, Any> {
+            pluginResult = CDVPluginResult(status: status, messageAs: message as! Dictionary)
+        } else if message is Int {
+            pluginResult = CDVPluginResult(status: status, messageAs: message as! Int)
+        } else if message is Double {
+            pluginResult = CDVPluginResult(status: status, messageAs: message as! Double)
+        } else {
+            pluginResult = CDVPluginResult(status: status)
+        }
+        pluginResult.setKeepCallbackAs(keepCallback)
+        commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    }
+}
